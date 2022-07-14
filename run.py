cf8fb13a4b993d63d5cbaf51da08a585f633aa2d
AugustaLiu <107892625+AugustaLiu@users.noreply.github.com>
2022-07-14 17:43:12 -0400

    Add files via upload

---
+++ run.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request, render_template
+import json
+import numpy as np
+import pickle
+
+
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    pred = ""
+    if request.method == "POST":
+        ses_rec = request.form["ses_rec"]
+        ses_n = request.form["ses_n"]
+        int_n = request.form["int_n"]
+        X = np.array([[float(ses_rec), float(ses_n), float(int_n)]])
+        pred = model.predict_proba(X)[0][1]
+    return render_template("index.html", pred=pred)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='127.0.0.1', port=5000)
